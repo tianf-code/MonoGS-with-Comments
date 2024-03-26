cfaec2364a6d1cac7e1fe5abd54a81bc46c9b748
Feng Tian <fetian@ethz.ch>
2024-03-26 20:09:37 +0100

    deleted:    screenshots/2024-03-26-11-05-09/screenshot-gui.png 	deleted:    screenshots/2024-03-26-11-05-09/screenshot.png 	deleted:    screenshots/2024-03-26-11-08-47/screenshot-gui.png 	deleted:    screenshots/2024-03-26-11-08-47/screenshot.png 	deleted:    screenshots/2024-03-26-11-09-10/screenshot-gui.png 	deleted:    screenshots/2024-03-26-11-09-10/screenshot.png 	deleted:    screenshots/2024-03-26-11-09-48/screenshot-gui.png 	deleted:    screenshots/2024-03-26-11-09-48/screenshot.png 	modified:   slam.py

--- slam.py
+++ slam.py
@@ -56,40 +56,52 @@ class SLAM:
         # Initialize 3D Gaussian Splatting Model
         self.gaussians = GaussianModel(model_params.sh_degree, config=self.config)
         self.gaussians.init_lr(6.0)
+
+        # Load dataset
         self.dataset = load_dataset(
             model_params, model_params.source_path, config=config
         )
 
-        self.gaussians.training_setup(opt_params)
-        bg_color = [0, 0, 0]
-        self.background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+        self.gaussians.training_setup(opt_params)   # Set optimization parameters to Gaussian models
+        bg_color = [0, 0, 0]    # Set background color: Black
+        self.background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")    # Put background color tensor on cuda
 
+        # Create two multi-process queues for communication between the front end and back end
         frontend_queue = mp.Queue()
         backend_queue = mp.Queue()
 
+        # According to whether to use the GUI, choose to use mp.Queue() or FakeQueue() to create a fake queue object
+        # If the program needs to communicate with the GUI process, use a real multi-process queue;
+        # Use a fake queue if not required to avoid throwing exceptions without a GUI.
         q_main2vis = mp.Queue() if self.use_gui else FakeQueue()
         q_vis2main = mp.Queue() if self.use_gui else FakeQueue()
 
+        # Update configs
         self.config["Results"]["save_dir"] = save_dir
         self.config["Training"]["monocular"] = self.monocular
 
+        # Create front-end and back-end instances and set parameters
         self.frontend = FrontEnd(self.config)
         self.backend = BackEnd(self.config)
 
+        # Background color, pipeline parameters, frontend queue and backend queue are shared between the front end and back end
+        # Dataset is in frontend
         self.frontend.dataset = self.dataset
         self.frontend.background = self.background
         self.frontend.pipeline_params = self.pipeline_params
         self.frontend.frontend_queue = frontend_queue
         self.frontend.backend_queue = backend_queue
+        # GUI is in frontend
         self.frontend.q_main2vis = q_main2vis
         self.frontend.q_vis2main = q_vis2main
         self.frontend.set_hyperparams()
 
+        # Gaussians and optimization are in back end
         self.backend.gaussians = self.gaussians
-        self.backend.background = self.background
         self.backend.cameras_extent = 6.0
-        self.backend.pipeline_params = self.pipeline_params
         self.backend.opt_params = self.opt_params
+        self.backend.background = self.background
+        self.backend.pipeline_params = self.pipeline_params
         self.backend.frontend_queue = frontend_queue
         self.backend.backend_queue = backend_queue
         self.backend.live_mode = self.live_mode
@@ -104,27 +116,34 @@ class SLAM:
             q_vis2main=q_vis2main,
         )
 
-        backend_process = mp.Process(target=self.backend.run)
+        # Start SLAM
+        backend_process = mp.Process(target=self.backend.run)   # Create a background process and set target function as self.backend.run()
         if self.use_gui:
             gui_process = mp.Process(target=slam_gui.run, args=(self.params_gui,))
             gui_process.start()
             time.sleep(5)
+        backend_process.start() # Start the backend
 
-        backend_process.start()
-        self.frontend.run()
+
+        self.frontend.run() # Start the frontend
         backend_queue.put(["pause"])
 
+        # SLAM ends, record end time
         end.record()
-        torch.cuda.synchronize()
+
+        # Evaluation
+        torch.cuda.synchronize()    # Synchronize the CPU and GPU to ensure that calculations on the GPU are completed
         # empty the frontend queue
-        N_frames = len(self.frontend.cameras)
-        FPS = N_frames / (start.elapsed_time(end) * 0.001)
+        N_frames = len(self.frontend.cameras)   # Number of camera frames processed by the front end
+        FPS = N_frames / (start.elapsed_time(end) * 0.001)  # Calculate the frame rate.
         Log("Total time", start.elapsed_time(end) * 0.001, tag="Eval")
         Log("Total FPS", N_frames / (start.elapsed_time(end) * 0.001), tag="Eval")
 
         if self.eval_rendering:
-            self.gaussians = self.frontend.gaussians
-            kf_indices = self.frontend.kf_indices
+            self.gaussians = self.frontend.gaussians    # Gaussians after front-end processing
+            kf_indices = self.frontend.kf_indices   # keyframes indices
+
+            # Calculate RMSE Absolute Trajectory Error (ATE)
             ATE = eval_ate(
                 self.frontend.cameras,
                 self.frontend.kf_indices,
@@ -134,6 +153,7 @@ class SLAM:
                 monocular=self.monocular,
             )
 
+            # Evaluate rendering results (Before optimization)
             rendering_result = eval_rendering(
                 self.frontend.cameras,
                 self.gaussians,
@@ -155,10 +175,14 @@ class SLAM:
                 FPS,
             )
 
+            # Then, prepare to evalute the rendering results after optimization
             # re-used the frontend queue to retrive the gaussians from the backend.
-            while not frontend_queue.empty():
+            while not frontend_queue.empty():   # Empty frontend queue
                 frontend_queue.get()
             backend_queue.put(["color_refinement"])
+
+            # Wait in an infinite loop to obtain synchronized signal from the front-end queue
+            # and get the optimized Gaussians from the back-end queue
             while True:
                 if frontend_queue.empty():
                     time.sleep(0.01)
@@ -166,9 +190,10 @@ class SLAM:
                 data = frontend_queue.get()
                 if data[0] == "sync_backend" and frontend_queue.empty():
                     gaussians = data[1]
-                    self.gaussians = gaussians
+                    self.gaussians = gaussians  # Get optimized Gaussians
                     break
 
+            # Evaluate rendering results (After optimization)
             rendering_result = eval_rendering(
                 self.frontend.cameras,
                 self.gaussians,
@@ -188,16 +213,18 @@ class SLAM:
                 FPS,
             )
             wandb.log({"Metrics": metrics_table})
-            save_gaussians(self.gaussians, self.save_dir, "final_after_opt", final=True)
+            save_gaussians(self.gaussians, self.save_dir, "final_after_opt", final=True)    # Save optimized Gaussians
 
+        # End backend and GUI process
         backend_queue.put(["stop"])
-        backend_process.join()
+        backend_process.join()  # Wait for background process to complete
         Log("Backend stopped and joined the main thread")
         if self.use_gui:
             q_main2vis.put(gui_utils.GaussianPacket(finish=True))
             gui_process.join()
             Log("GUI Stopped and joined the main thread")
 
+    # Fake function
     def run(self):
         pass
 
